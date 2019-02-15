8311de86dcc4138a3db7910adaa74a3769313258
MikiKru <michal_kruczkowski@o2.pl>
2019-02-15 12:04:24 +0100

    iw5

---
+++ src/iw5/Iw5.java
@@ -0,0 +1,29 @@
+package iw5;
+
+public class Iw5 {
+    public static void main(String[] args) {
+        int decimalNum = 3;
+        switch(decimalNum){
+            case 0:
+                System.out.println("0");
+                break;
+            case 1:
+                System.out.println("I");
+                break;
+            case 2:
+                System.out.println("II");
+                break;
+            case 3:
+                System.out.println("III");
+                break;
+            case 4:
+                System.out.println("IV");
+                break;
+            case 5:
+                System.out.println("V");
+                break;
+            default:
+                System.out.println("Nie ma takiej liczby");
+        }
+    }
+}
